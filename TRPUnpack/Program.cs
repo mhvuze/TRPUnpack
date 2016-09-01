0b172061d29edff0eb5330fb11ac5d467e3db3d4
mhvuze <vuze.mhf2resg@gmail.com>
2016-09-01 22:53:17 +0200

    Initial commit

--- TRPUnpack/Program.cs
+++ TRPUnpack/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TRPUnpack
 {
@@ -10,6 +11,21 @@ namespace TRPUnpack
     {
         static void Main(string[] args)
         {
+            // Variables
+            string input = "";
+
+            // Print header
+            Console.WriteLine("TRPUnpack by MHVuze");
+            Console.WriteLine("=========================");
+
+            // Handle arguments
+            if (args.Length < 1) { Console.WriteLine("ERROR: Supply input file."); return; }
+            input = args[0];
+
+            // Check file
+            if (!File.Exists(input)) { Console.WriteLine("ERROR: Specified file doesn't exist."); return; }
+
+
         }
     }
 }
